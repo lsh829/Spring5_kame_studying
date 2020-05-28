2d226b867bcd7575abf9b67a6d0491136ca20b3c
lsh <tjsghksusan@naver.com>
2020-05-28 18:31:59 +0900

    DB연동 대체

---
+++ sp5-chap03/src/main/java/spring/MemberDao.java
@@ -0,0 +1,23 @@
+package spring;
+
+import java.util.HashMap;
+import java.util.Map;
+// 원래는 DB를 연동해야하는 클래스 이다. 
+public class MemberDao {
+	private static long nextId = 0;
+	
+	private Map<String, Member> map = new HashMap<>();
+	
+	public Member selectByEmail(String email) {
+		return map.get(email);
+	}
+	
+	public void insert(Member member) {
+		member.setId(++nextId);
+		map.put(member.getEmail(), member);
+	}
+	
+	public void update(Member member) {
+		map.put(member.getEmail(), member);
+	}
+}
