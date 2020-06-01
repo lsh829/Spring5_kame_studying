c401bec577356169bf91f102318db28dcf9596fc
lsh <tjsghksusan@naver.com>
2020-06-01 14:51:11 +0900

    스프링을 사용한 코드, 스프링이 어떤 객체를 생성하고 의존을 어떻게 주입할지를 정의한 설정 정보

---
+++ sp5-chap03/src/main/java/config/AppCtx.java
@@ -0,0 +1,29 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import spring.ChangePasswordService;
+import spring.MemberDao;
+import spring.MemberRegisterService;
+
+@Configuration // 스프링 설정 클래스 - 이 애노테이션을 붙여야 스프링 설정 클래스로 사용할 수 있다. 
+public class AppCtx {
+	@Bean // 각각의 메서도마다 하나의 빈 객체를 생성 
+	public MemberDao memberDao() {
+		return new MemberDao();
+	}
+	
+	@Bean
+	public MemberRegisterService memberRegSvc() {
+		return new MemberRegisterService(memberDao());
+	}
+	
+	@Bean
+	public ChangePasswordService changePwdSvc() {
+		ChangePasswordService pwdSvc = new ChangePasswordService();
+		pwdSvc.setMemberDao(memberDao()); //setter 메소드를 이용해서 의존 객체를 주입.
+		return pwdSvc;
+	}
+}
+
